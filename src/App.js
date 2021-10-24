f5b22222129d4092e2f98505b56ee82f0ae27237
thanhhoan <pdth456@gmail.com>
2021-10-24 16:50:14 +0700

    first commit 🚀

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import DropDown from "./components/DropDown";
+
+import BubbleSort from "./sortingAlgorithms/BubbleSort";
+import SelectionSort from "./sortingAlgorithms/SelectionSort";
+
+export default function App() {
+  const [userSelection, setUserSelection] = useState("");
+
+  return (
+    <>
+      <div className="w-screen h-auto flex flex-col items-center bg-black">
+        <div className="mb-20">
+          <h1 className="text-5xl text-white text-center mt-10 font-bold">
+            Sorting Algorithms &nbsp;
+            <span className="bg-white text-black rounded-lg p-1">
+              Visualizer
+            </span>
+          </h1>
+        </div>
+        <DropDown userSelection={(name) => setUserSelection(name)} />
+        <main className="w-full p-5">
+          {userSelection === "Bubble Sort" && <BubbleSort />}
+          {userSelection === "Selection Sort" && <SelectionSort />}
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ src/utils/commons.js
@@ -0,0 +1,28 @@
+export const CANVAS_WIDTH = 1580;
+export const CANVAS_HEIGHT = 675;
+
+export const sorting = [
+  { name: "Bubble Sort" },
+  { name: "Quick Sort" },
+  { name: "Merge Sort" },
+  { name: "Selection Sort" },
+  { name: "Radix Sort" },
+  { name: "Heap Sort" },
+  { name: "Counting Sort" },
+  { name: "Bucket Sort" },
+  { name: "Shell Sort" },
+  { name: "Tim Sort" },
+  { name: "Comb Sort" },
+  { name: "Pigeonhole Sort" },
+  { name: "Cycle Sort" },
+  { name: "Cocktail Sort" },
+  { name: "Strand Sort" },
+  { name: "Bitonic Sort" },
+  { name: "Binary Insertion Sort" },
+  { name: "BogoSort or Permutation Sort" },
+  { name: "Gnome Sort" },
+  { name: "Sleep Sort" },
+  { name: "Stooge Sort" },
+  { name: "Tree Sort" },
+  { name: "Cartesian Tree Sort" },
+];
